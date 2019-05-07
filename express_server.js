b76d0ff0a6699c2fb92b3007f5842ddb511d4849
Dan <dan.zajac@mac.com>
2019-05-07 21:44:55 +0000

    Added links to urls_index in HTML form with links

--- express_server.js
+++ express_server.js
@@ -9,15 +9,24 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 app.get("/urls.json", (req, res) => {
   res.send(urlDatabase);
 });
 app.get("/faq", (req, res) => {
   res.send("FAQ");
 });
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };   //When sending variables to and EJS template, we need to have them inside an object
+  res.render("urls_index", templateVars);
+});
+app.get("/hello", (req, res) => {
+  let templateVars = { greeting: 'Hello World!!!!' }; //Send "Hello World!!!!" as the variable templateVars to the template "hello_world by re.render()"
+  res.render("hello_world", templateVars);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
