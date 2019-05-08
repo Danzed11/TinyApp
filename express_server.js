67d9866174bdc8819108aeb52c2936eaf5819781
Dan <dan.zajac@mac.com>
2019-05-08 02:22:19 +0000

    Added POST form, with path, and action POST

--- express_server.js
+++ express_server.js
@@ -1,8 +1,22 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
+function generateRandomString(length) {
+  let letters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
+  let result = "";
+  let charactersLength = letters.length;
+  for(let i = 0; i < length; i++) {
+    result += letters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+  }
+  console.log(generateRandomString(6));
+  
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -25,6 +39,19 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };   //When sending variables to and EJS template, we need to have them inside an object
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => { //submission form
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok man");         // Respond with 'Ok' (we will replace this) in the BROWSER >> CLIENT
 });
 
 app.get("/urls/:shortURL", (req, res) => {
